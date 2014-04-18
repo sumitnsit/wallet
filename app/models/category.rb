0f9b927eb49c569d458c145f476b2762c1fd2ec1
Sumit Kapoor <sumit.india@gmail.com>
2014-04-18 09:16:15 +0530

    category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ActiveRecord::Base
   belongs_to :parent, :class_name => 'Category', :foreign_key => :parent_id
   scope :active, -> { where("active IS true") }	
-  scope :main, -> { where("parent_id = 1") }	
+  scope :main, -> { where("parent_id = 1") }
+  scope :order, order("parent_id")
+
 end
